282d04276d7c8f8b0441c489712c381c305c2128
zarix908 <daniyarrus@gmail.com>
2023-07-03 23:37:57 +0500

    create descripter of registers

--- src/debugger.rs
+++ src/debugger.rs
@@ -18,7 +18,7 @@ use nix::{
 use crate::{
     breakpoint::Breakpoint,
     dwarf::Dwarf,
-    reg::{Reg, RegSelector},
+    reg::{self, Reg, RegSelector},
 };
 
 pub struct Debugger<'a> {
@@ -149,73 +149,17 @@ impl<'a> Debugger<'a> {
     }
 
     fn get_register_value(&self, reg: &RegSelector) -> Result<u64, String> {
-        let regs = ptrace::getregs(Pid::from_raw(self.program_pid))
+        let mut regs = ptrace::getregs(Pid::from_raw(self.program_pid))
             .map_err(|e| format!("failed to get regs: {}", e))?;
 
-        Ok(match reg {
-            RegSelector::Reg(Reg::R15) | RegSelector::Name("r15") => regs.r15,
-            RegSelector::Reg(Reg::R14) | RegSelector::Name("r14") => regs.r14,
-            RegSelector::Reg(Reg::R13) | RegSelector::Name("r13") => regs.r13,
-            RegSelector::Reg(Reg::R12) | RegSelector::Name("r12") => regs.r12,
-            RegSelector::Reg(Reg::RBP) | RegSelector::Name("rbp") => regs.rbp,
-            RegSelector::Reg(Reg::RBX) | RegSelector::Name("rbx") => regs.rbx,
-            RegSelector::Reg(Reg::R11) | RegSelector::Name("r11") => regs.r11,
-            RegSelector::Reg(Reg::R10) | RegSelector::Name("r10") => regs.r10,
-            RegSelector::Reg(Reg::R9) | RegSelector::Name("r9") => regs.r9,
-            RegSelector::Reg(Reg::R8) | RegSelector::Name("r8") => regs.r8,
-            RegSelector::Reg(Reg::RAX) | RegSelector::Name("rax") => regs.rax,
-            RegSelector::Reg(Reg::RCX) | RegSelector::Name("rcx") => regs.rcx,
-            RegSelector::Reg(Reg::RDX) | RegSelector::Name("rdx") => regs.rdx,
-            RegSelector::Reg(Reg::RSI) | RegSelector::Name("rsi") => regs.rsi,
-            RegSelector::Reg(Reg::RDI) | RegSelector::Name("rdi") => regs.rdi,
-            RegSelector::Reg(Reg::RIP) | RegSelector::Name("rip") => regs.rip,
-            RegSelector::Reg(Reg::CS) | RegSelector::Name("cs") => regs.cs,
-            RegSelector::Reg(Reg::EFLAGS) | RegSelector::Name("eflags") => regs.eflags,
-            RegSelector::Reg(Reg::RSP) | RegSelector::Name("rsp") => regs.rsp,
-            RegSelector::Reg(Reg::SS) | RegSelector::Name("ss") => regs.ss,
-            RegSelector::Reg(Reg::FSBASE) | RegSelector::Name("fsbase") => regs.fs_base,
-            RegSelector::Reg(Reg::GSBASE) | RegSelector::Name("gsbase") => regs.gs_base,
-            RegSelector::Reg(Reg::DS) | RegSelector::Name("ds") => regs.ds,
-            RegSelector::Reg(Reg::ES) | RegSelector::Name("es") => regs.es,
-            RegSelector::Reg(Reg::FS) | RegSelector::Name("fs") => regs.fs,
-            RegSelector::Reg(Reg::GS) | RegSelector::Name("gs") => regs.gs,
-            _ => Err("register not found")?,
-        })
+        Ok(reg::get_register_value(&mut regs, reg))
     }
 
     fn set_register_value(&self, reg: &RegSelector, value: u64) -> Result<(), String> {
         let mut regs = ptrace::getregs(Pid::from_raw(self.program_pid))
             .map_err(|e| format!("failed to get regs: {}", e))?;
 
-        match reg {
-            RegSelector::Reg(Reg::R15) | RegSelector::Name("r15") => regs.r15 = value,
-            RegSelector::Reg(Reg::R14) | RegSelector::Name("r14") => regs.r14 = value,
-            RegSelector::Reg(Reg::R13) | RegSelector::Name("r13") => regs.r13 = value,
-            RegSelector::Reg(Reg::R12) | RegSelector::Name("r12") => regs.r12 = value,
-            RegSelector::Reg(Reg::RBP) | RegSelector::Name("rbp") => regs.rbp = value,
-            RegSelector::Reg(Reg::RBX) | RegSelector::Name("rbx") => regs.rbx = value,
-            RegSelector::Reg(Reg::R11) | RegSelector::Name("r11") => regs.r11 = value,
-            RegSelector::Reg(Reg::R10) | RegSelector::Name("r10") => regs.r10 = value,
-            RegSelector::Reg(Reg::R9) | RegSelector::Name("r9") => regs.r9 = value,
-            RegSelector::Reg(Reg::R8) | RegSelector::Name("r8") => regs.r8 = value,
-            RegSelector::Reg(Reg::RAX) | RegSelector::Name("rax") => regs.rax = value,
-            RegSelector::Reg(Reg::RCX) | RegSelector::Name("rcx") => regs.rcx = value,
-            RegSelector::Reg(Reg::RDX) | RegSelector::Name("rdx") => regs.rdx = value,
-            RegSelector::Reg(Reg::RSI) | RegSelector::Name("rsi") => regs.rsi = value,
-            RegSelector::Reg(Reg::RDI) | RegSelector::Name("rdi") => regs.rdi = value,
-            RegSelector::Reg(Reg::RIP) | RegSelector::Name("rip") => regs.rip = value,
-            RegSelector::Reg(Reg::CS) | RegSelector::Name("cs") => regs.cs = value,
-            RegSelector::Reg(Reg::EFLAGS) | RegSelector::Name("eflags") => regs.eflags = value,
-            RegSelector::Reg(Reg::RSP) | RegSelector::Name("rsp") => regs.rsp = value,
-            RegSelector::Reg(Reg::SS) | RegSelector::Name("ss") => regs.ss = value,
-            RegSelector::Reg(Reg::FSBASE) | RegSelector::Name("fsbase") => regs.fs_base = value,
-            RegSelector::Reg(Reg::GSBASE) | RegSelector::Name("gsbase") => regs.gs_base = value,
-            RegSelector::Reg(Reg::DS) | RegSelector::Name("ds") => regs.ds = value,
-            RegSelector::Reg(Reg::ES) | RegSelector::Name("es") => regs.es = value,
-            RegSelector::Reg(Reg::FS) | RegSelector::Name("fs") => regs.fs = value,
-            RegSelector::Reg(Reg::GS) | RegSelector::Name("gs") => regs.gs = value,
-            _ => Err("register not found")?,
-        };
+        reg::set_register_value(&mut regs, reg, value);
 
         ptrace::setregs(Pid::from_raw(self.program_pid), regs)
             .map_err(|e| format!("failed to set regs: {}", e))?;
@@ -223,46 +167,11 @@ impl<'a> Debugger<'a> {
     }
 
     fn dump_registers(&self) -> Result<(), String> {
-        let regs = ptrace::getregs(Pid::from_raw(self.program_pid))
+        let mut regs = ptrace::getregs(Pid::from_raw(self.program_pid))
             .map_err(|e| format!("failed to get regs: {}", e))?;
 
-        let regs = [
-            regs.r15,
-            regs.r14,
-            regs.r13,
-            regs.r12,
-            regs.rbp,
-            regs.rbx,
-            regs.r11,
-            regs.r10,
-            regs.r9,
-            regs.r8,
-            regs.rax,
-            regs.rcx,
-            regs.rdx,
-            regs.rsi,
-            regs.rdi,
-            regs.rip,
-            regs.cs,
-            regs.eflags,
-            regs.rsp,
-            regs.ss,
-            regs.fs_base,
-            regs.gs_base,
-            regs.ds,
-            regs.es,
-            regs.fs,
-            regs.gs,
-        ];
-
-        let names = [
-            "r15", "r14", "r13", "r12", "rbp", "rbx", "r11", "r10", "r9", "r8", "rax", "rcx",
-            "rdx", "rsi", "rdi", "rip", "cs", "eflags", "rsp", "ss", "fsbase", "gsbase", "ds",
-            "es", "fs", "gs",
-        ];
-
-        for i in 0..26 {
-            println!("{}: {:#X}", names[i], regs[i]);
+        for (name, value) in reg::dump_registers(&mut regs) {
+            println!("{}: {:#X}", name, value);
         }
 
         Ok(())
--- src/reg.rs
+++ src/reg.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+#[derive(PartialEq, Eq)]
 #[allow(dead_code)]
 pub enum Reg {
     R15,
@@ -28,7 +31,194 @@ pub enum Reg {
     GS,
 }
 
+struct RegDescriptor<'value, 'name> {
+    reg: Reg,
+    name: &'name str,
+    value_ptr: &'value mut u64,
+}
+
 pub enum RegSelector<'a> {
     Reg(Reg),
     Name(&'a str),
+}
+
+pub fn set_register_value(
+    regs: &mut nix::libc::user_regs_struct,
+    selector: &RegSelector,
+    value: u64,
+) {
+    select_regs(regs, Some(selector), Some(value));
+}
+
+pub fn get_register_value(regs: &mut nix::libc::user_regs_struct, selector: &RegSelector) -> u64 {
+    *select_regs(regs, Some(selector), None)
+        .unwrap()
+        .values()
+        .into_iter()
+        .last()
+        .unwrap()
+}
+
+pub fn dump_registers(regs: &mut nix::libc::user_regs_struct) -> HashMap<String, u64> {
+    select_regs(regs, None, None).unwrap()
+}
+
+fn select_regs(
+    regs: &mut nix::libc::user_regs_struct,
+    selector: Option<&RegSelector>,
+    set_value: Option<u64>,
+) -> Option<HashMap<String, u64>> {
+    let descpriptors = [
+        RegDescriptor {
+            reg: Reg::R15,
+            name: "r15",
+            value_ptr: &mut regs.r15,
+        },
+        RegDescriptor {
+            reg: Reg::R14,
+            name: "r14",
+            value_ptr: &mut regs.r14,
+        },
+        RegDescriptor {
+            reg: Reg::R13,
+            name: "r13",
+            value_ptr: &mut regs.r13,
+        },
+        RegDescriptor {
+            reg: Reg::R12,
+            name: "r12",
+            value_ptr: &mut regs.r12,
+        },
+        RegDescriptor {
+            reg: Reg::RBP,
+            name: "rbp",
+            value_ptr: &mut regs.rbp,
+        },
+        RegDescriptor {
+            reg: Reg::RBX,
+            name: "rbx",
+            value_ptr: &mut regs.rbx,
+        },
+        RegDescriptor {
+            reg: Reg::R11,
+            name: "r11",
+            value_ptr: &mut regs.r11,
+        },
+        RegDescriptor {
+            reg: Reg::R10,
+            name: "r10",
+            value_ptr: &mut regs.r10,
+        },
+        RegDescriptor {
+            reg: Reg::R9,
+            name: "r9",
+            value_ptr: &mut regs.r9,
+        },
+        RegDescriptor {
+            reg: Reg::R8,
+            name: "r8",
+            value_ptr: &mut regs.r8,
+        },
+        RegDescriptor {
+            reg: Reg::RAX,
+            name: "rax",
+            value_ptr: &mut regs.rax,
+        },
+        RegDescriptor {
+            reg: Reg::RCX,
+            name: "rcx",
+            value_ptr: &mut regs.rcx,
+        },
+        RegDescriptor {
+            reg: Reg::RDX,
+            name: "rdx",
+            value_ptr: &mut regs.rdx,
+        },
+        RegDescriptor {
+            reg: Reg::RSI,
+            name: "rsi",
+            value_ptr: &mut regs.rsi,
+        },
+        RegDescriptor {
+            reg: Reg::RDI,
+            name: "rdi",
+            value_ptr: &mut regs.rdi,
+        },
+        RegDescriptor {
+            reg: Reg::RIP,
+            name: "rip",
+            value_ptr: &mut regs.rip,
+        },
+        RegDescriptor {
+            reg: Reg::CS,
+            name: "cs",
+            value_ptr: &mut regs.cs,
+        },
+        RegDescriptor {
+            reg: Reg::EFLAGS,
+            name: "eflags",
+            value_ptr: &mut regs.eflags,
+        },
+        RegDescriptor {
+            reg: Reg::RSP,
+            name: "rsp",
+            value_ptr: &mut regs.rsp,
+        },
+        RegDescriptor {
+            reg: Reg::SS,
+            name: "ss",
+            value_ptr: &mut regs.ss,
+        },
+        RegDescriptor {
+            reg: Reg::FSBASE,
+            name: "fsbase",
+            value_ptr: &mut regs.fs_base,
+        },
+        RegDescriptor {
+            reg: Reg::GSBASE,
+            name: "gsbase",
+            value_ptr: &mut regs.gs_base,
+        },
+        RegDescriptor {
+            reg: Reg::DS,
+            name: "ds",
+            value_ptr: &mut regs.ds,
+        },
+        RegDescriptor {
+            reg: Reg::ES,
+            name: "es",
+            value_ptr: &mut regs.es,
+        },
+        RegDescriptor {
+            reg: Reg::FS,
+            name: "fs",
+            value_ptr: &mut regs.fs,
+        },
+        RegDescriptor {
+            reg: Reg::GS,
+            name: "gs",
+            value_ptr: &mut regs.gs,
+        },
+    ];
+
+    let mut result = HashMap::new();
+    for decriptor in descpriptors {
+        match &selector {
+            Some(RegSelector::Reg(reg)) if decriptor.reg != *reg => continue,
+            Some(RegSelector::Name(name)) if decriptor.name != *name => continue,
+            _ => (),
+        }
+
+        match set_value {
+            Some(value) => {
+                *decriptor.value_ptr = value;
+                break;
+            }
+            None => {
+                result.insert(decriptor.name.to_owned(), *decriptor.value_ptr);
+            }
+        }
+    }
+
+    Some(result)
 }
